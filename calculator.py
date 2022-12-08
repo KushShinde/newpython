a8491c392d6222cc265cb90297c9854e9b716e7a
KushShinde <kushshinde111@gmail.com>
2022-12-08 14:39:44 +0530

    this is the first commit

---
+++ calculator.py
@@ -0,0 +1,28 @@
+# def addition(num1,num2):
+#     num1 += num2
+#     return num1
+# def subtraction(num1,num2):
+#     num1 -= num2
+#     return num1
+# def mul(num1,num2):
+#     num1 *= num2
+#     return num1
+# def division(num1,num2):
+#     num1 /= num2
+#     return num1
+#
+# def module(num1,num2):
+#     num1 %= num2
+#     return num1
+#
+# def default(num1,num2):
+#     return "Incorrect day"
+#
+# addition(2,4)
+# subtraction(4,5)
+# mul(2,2)
+# division(4,4)
+# module(5,3)
+#
+
+print("Hello world")
